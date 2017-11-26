3d07732209a330ace61aa950074ee67dae6fed2e
Cecemelpvp <cedricthauvoye1@gmail.com>
2017-11-26 18:25:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,14 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = "MzgyOTkyOTQyMDkwMDI3MDM5.DPdyMA.acOBvno_9osaMa2g2xCeW8JIkzY";
 const bot = new Discord.Client();
+const prefix = "."
 
+bot.on('guildMemberAdd', gMember => {
+    var reason = "C'est le rôlebpar default!";
+    //tools.welcomeUserWithImg(gMember.user, gMember.user.avatarURL);
+    tools.addRoleTo(gMember.user, usefulInfos.defaultRole, reason, gMember);
+    usefulInfos.welcomeChannel.send("Bienvenue sur un serveur ! Passes un agrébale moment !");
 
-
+});
 
 bot.login(token);
