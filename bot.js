53c5041a97e7e0c65edffd7b17689e08e26a7702
Cecemelpvp <cedricthauvoye1@gmail.com>
2017-11-26 19:36:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = "MzgyOTkyOTQyMDkwMDI3MDM5.DPdyMA.acOBvno_9osaMa2g2xCeW8JIkzY";
 const bot = new Discord.Client();
-const prefix = "."
+const prefix = ".";
 
 client.on('message', message => {	
 var msg = message;
