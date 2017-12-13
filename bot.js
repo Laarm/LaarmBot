ea09cb1a521f10673e91931cefa3e996522ccce9
Cecemelpvp <cedricthauvoye1@gmail.com>
2017-12-13 18:13:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,27 +140,8 @@ mainMessage.delete()
 }	
 
   
-  if (message.content.startsWith(prefix + "eval")) {
-    if(message.author.id !== "252873409401323520") return;
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
 
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
 
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
-});
-	function clean(text) {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
-}
 })	      
 
 client.login(token)
